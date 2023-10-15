cb4d8810e3685f2a2f77f86e84e3f25e078eac48
Bohdan Yarish <yarisb01@kits.kfplc.com>
2023-10-15 18:11:03 +0300

    task-4 update frontend

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,6 +5,7 @@ export const ProductSchema = Yup.object({
   title: Yup.string().required().default(""),
   description: Yup.string().default(""),
   price: Yup.number().positive().required().defined().default(0),
+  count: Yup.number().positive().required().defined().default(0),
 });
 
 export const AvailableProductSchema = ProductSchema.shape({
