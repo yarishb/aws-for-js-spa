36cd70b56b814e0b2f2eaa8bff43741d87e132fa
Bohdan Yarish <yarisb01@kits.kfplc.com>
2023-10-22 19:10:31 +0300

    task-5 updates

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://yuxi0m18hk.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://9pxbooh2tb.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://yuxi0m18hk.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
